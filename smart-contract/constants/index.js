1e7bc24524ef902f3232a99ae10fbc5a0c980746
Tomiwa Adeyemi <adeyemitomiwa7@gmail.com>
2022-06-11 12:00:35 +0100

    Add files via upload

---
+++ smart-contract/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x02a71C1306FC970e1e1AAce60978de36ebb90EAa";
+// URL to extract Metadata for a KiwiNfts NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
